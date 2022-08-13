2affbb9fbce7895750c803643c64cd1266014ed7
Tra Van Thien <travanthien202@gmail.com>
2022-08-13 09:57:27 +0700

    Initialize project

---
+++ CRM_FE/src/main/java/com/crm/fe/CrmFeApplication.java
@@ -0,0 +1,13 @@
+package com.crm.fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmFeApplication.class, args);
+	}
+
+}
---
+++ CRM_FE/src/test/java/com/crm/fe/CrmFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.fe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
